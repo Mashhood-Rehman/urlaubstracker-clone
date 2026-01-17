583aff1ba3bea3bfc1707145c69230161961dccd
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2026-01-16 21:02:36 -0800

    ok

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,10 +11,10 @@ if (!connectionString) {
     throw new Error("DATABASE_URL is not set");
 }
 
-const adapter = new PrismaNeon({connectionString})
+const adapter = new PrismaNeon({ connectionString })
 
 const globalForPrisma = globalThis as any
-const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })  
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
